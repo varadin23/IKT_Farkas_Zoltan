2c6a644a86f2a6f1e008c39fe5bf4dce09207808
VaradiNiki <varadin@kkszki.hu>
2021-10-05 12:55:03 +0200

    login űrlap validálása kész

--- Projekt1/classes.php
+++ Projekt1/classes.php
@@ -11,11 +11,65 @@ class Forms{
 class  LoginCheck extends Forms{
 //különböző hia üzenetek tárolása
 
+function __construct($name, $password){
+    $this->uName=$name;
+    $this->uPass=$password;
+
+    if (empty($name)){
+        $this->uNameErr="A név mező nem lehet üres!";
+    }
+    else if(!preg_match("/^[a-zA-Z-']*$/",$name)){
+        $this->uNameErr="Nem megfelelő formátum!";
+    }
+    else{
+        //$this->uName=$name;
+        $this->uNameErr="";
+    }
+
+    $num=8-strlen($password);
+    if(empty($password)){
+        $this->uPassErr="Jelszó mező nem lehet üres!";
+    }
+    else if (strlen($password)<8){
+        $this->uPassErr="Jelszónak legalább 8 karakternek kell lennie. Még: $num karakternek kell lennie!";
+    }
 }
+
+    public function getUPass()
+    {
+        return $this->uPass;
+    }
+
+ 
+    public function getUPassErr()
+    {
+        return $this->uPassErr;
+    }
+
+    public function getUNameErr()
+    {
+        return $this->uNameErr;
+    }
+
+    public function getUName()
+    {
+        return $this->uName;
+    }
+}
+
 
 //regisztrációs űrlap beviteli mezőinek átnézése
 class RegCheck extends Forms{
 
+
+   
+
+   
+
+    /**
+     * Get the value of uName
+     */ 
+   
 }
 
 ?>
--- Projekt1/login.php
+++ Projekt1/login.php
@@ -1,13 +1,25 @@
+<?php
+  include('classes.php');
+  if(isset($_POST['userName1']) && isset($_POST['userPass1'])){
+    $log=new LoginCheck($_POST['userName1'], $_POST['userPass1']);
+  }
+  else{
+    $log=new LoginCheck("", "");
+  }
+  
+?>
+
 <h2>Bejelentkezés</h2>
-<form>
+<form action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST">
   <div class="form-group">
-    <label for="uname">Felhasználónév</label>
-    <input type="text" class="form-control" id="exampleInputUsername1" aria-describedby="usernameHelp" placeholder="Gépeld be a neved">
-    <small id="emailHelp" class="form-text text-muted">A felhasználó neved ékezetet nem tartalmazhat!</small>
+    <label>Felhasználónév</label>
+    <input type="text" class="form-control" name="userName1" value="<?php echo $log->getuName();?>" placeholder="Gépeld be a neved">
+    <small class="form-text text-muted" name="userInfo1"><?php echo $log->getUNameErr();?></small>
   </div>
   <div class="form-group">
-    <label for="pass">Jelszó</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Ide jöhet a jelszavad!">
+    <label>Jelszó</label>
+    <input type="password" class="form-control" name="userPass1" placeholder="Ide jöhet a jelszavad!">
+    <small class="form-text text-muted" name="userInfo1"><?php echo $log->getUPassErr();?></small>
   </div>
   <div class="form-group">
     <a href="index.php?id=reglap" style="color:white">Regisztráció</a>
